58d07db1c807b99f8645755a901cfb06bf6c01b0
Sukharev Maxim <maxim.sukharev@lazada.com>
2015-11-18 17:27:32 +0700

    add fs cache

--- index.js
+++ index.js
@@ -1,16 +1,34 @@
 'use strict';
 
 var isparta = require('isparta');
+var loaderUtils = require('loader-utils');
+var cache = require('./lib/fs-cache');
 
 module.exports = function(source) {
     var instrumenter = new isparta.Instrumenter({
         embedSource: true,
         noAutoWrap: true
     });
+    var options = loaderUtils.parseQuery(this.query);
+    var cacheDirectory = options.cacheDirectory;
+    var callback;
 
     if (this.cacheable) {
         this.cacheable();
     }
 
-    return instrumenter.instrumentSync(source, this.resourcePath);
+    if (cacheDirectory) {
+        callback = this.async();
+        return cache({
+            cacheDirectory: cacheDirectory,
+            resourcePath: this.resourcePath,
+            source: source,
+            instrumenter: instrumenter,
+        }, function(err, result) {
+            if (err) { return callback(err); }
+            return callback(null, result);
+        });
+    } else {
+        return instrumenter.instrumentSync(source, this.resourcePath);
+    }
 };
